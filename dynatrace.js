c87a7813c468371dc7934c5dd84f86bb18cacd81
Naveen Kotapati(UST <285487@ust.com>
2025-07-25 19:40:26 +0530

    dynatrace onfig

---
+++ dynatrace.js
@@ -0,0 +1,9 @@
+// (function(){
+//     var dT_ = window.dT_ || {};
+//     dT_.applicationId = "CUSTOM_APPLICATION-0DE5C9CB338B4BE5";
+//     dT_.agentUri = "https://ewx86657.live.dynatrace.com/jstag/agent/js/bootstrap.js";
+//     var script = document.createElement("script");
+//     script.src = dT_.agentUri;
+//     document.head.appendChild(script);
+//   })();
+  
